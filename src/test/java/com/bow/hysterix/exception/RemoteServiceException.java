6096d1a158c50639da3fe239066118902db6d013
vv <550456590@qq.com>
2018-03-25 19:05:10 +0800

    hystrix

---
+++ src/test/java/com/bow/hysterix/exception/RemoteServiceException.java
@@ -0,0 +1,10 @@
+package com.bow.hysterix.exception;
+
+public class RemoteServiceException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public RemoteServiceException(String message){
+		super(message);
+	}
+}
